ad0282d04e56101eacdaa8faacae3a0e29fe484e
smuratovic3 <smuratovic3@etf.unsa.ba>
2021-11-25 22:39:43 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
